f226ef56a54e28e98f11857980b6bff84dd3f340
Rafa Pardal <rafapardal07@hotmail.com>
2017-04-17 02:12:25 +0100

    add eslint airbnb

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,43 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-gulp.task('css', function() {
+gulp.task('css', () => {
     return gulp.src('src/sass/**/*.scss') // qualquer ficheiro scss na pasta sass ou subpastas
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError)) // minifica o css e mostra os erros dos ficheiros scss
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // minifica o css e mostra os erros dos ficheiros scss
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'] // prefix para browsers antigos ou MS
+            browsers: ['last 2 versions'], // prefix para browsers antigos ou MS
         }))
         .pipe(sourcemaps.write('.maps')) // cria o ficheiro de erro
         .pipe(gulp.dest('dist/css')) // coloca os ficheiros css nesta pasta
-        .pipe(browserSync.stream()) // fazer refresh na página depois do ficheiro ser atualizado
+        .pipe(browserSync.stream()); // fazer refresh na página depois do ficheiro ser atualizado
 });
 
-gulp.task('images', function() {
+gulp.task('images', () => {
     return gulp.src('src/images/*')
         .pipe(imagemin()) // minifica as imagens
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('copy', function(){
+gulp.task('copy', () => {
     return gulp.src('src/*.html')
         .pipe(gulp.dest('dist'))
-        .pipe(browserSync.stream()) // fazer refresh na página depois do ficheiro ser atualizado
+        .pipe(browserSync.stream()); // fazer refresh na página depois do ficheiro ser atualizado
 });
 
-gulp.task('browserSync', function(){
+gulp.task('browserSync', () => {
     browserSync.init({
         server: {
-            baseDir: 'dist'
-        }
-    })
+            baseDir: 'dist',
+        },
+    });
 });
 
-gulp.task('watch', ['browserSync', 'css', 'copy'], function(){
+gulp.task('watch', ['browserSync', 'css'], () => {
     gulp.watch('src/sass/**/*scss', ['css']);
     gulp.watch('src/*.html', ['copy']);
 });
